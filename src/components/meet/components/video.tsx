3a3b56f8097233183a51521b3df627fcaa8ad72e
THARUNJP <tharunroshini71@gmail.com>
2025-12-24 20:35:06 +0530

    video grid ui completed  and tested

--- src/components/meet/components/video.tsx
+++ src/components/meet/components/video.tsx
@@ -1,13 +1,34 @@
-export default function VideoGrid(){
+import { getGridClass } from "../../../lib/helper";
+import type { Participant } from "../../../types/types";
 
-    return(
-        <div className="h-[80vh]">
-        <div className="grid-col-3">
-            <div className="bg-red-500 h-40 w-40"></div>
-            <div className="bg-yellow-500 h-40 w-40"></div>
-            <div className="bg-red-500 h-40 w-40"></div>
-        </div>
-        </div>
-    )
+export default function VideoGrid() {
+  // Mock participants (UI only)
+  const participants: Participant[] = Array.from(
+    { length: 5 },
+    (_, i) => ({
+      id: String(i),
+      name: `User ${i + 1}`,
+    })
+  );
 
+  return (
+   <div className="flex-1 p-4 h-[calc(100vh-(4rem+3.5rem))]">
+      <div
+        className={`
+          grid gap-4 h-full
+          ${getGridClass(participants.length)}
+          place-items-center
+        `}
+      >
+        {participants.map((p) => (
+          <div
+            key={p.id}
+            className="w-full h-full bg-black rounded-lg flex items-center justify-center text-white"
+          >
+            {p.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
