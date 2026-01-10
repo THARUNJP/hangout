6135ead09f038da3f2d9d90d0eb155a7ac771e21
THARUNJP <tharunroshini71@gmail.com>
2026-01-10 17:36:48 +0530

    validation added in guard as well

--- src/components/meet/components/meetingGuard.tsx
+++ src/components/meet/components/meetingGuard.tsx
@@ -1,13 +1,19 @@
 import type { JSX } from "react";
 import { Navigate, useParams } from "react-router-dom";
+import { validateSession } from "../../../service/session.service";
+import { showHotToast } from "../../../lib/toast";
 
-export function MeetingGuard({ children }: { children: JSX.Element }) {
+export async function MeetingGuard({ children }: { children: JSX.Element }) {
   const { code } = useParams<{ code: string }>();
-
   if (!code) {
     return <Navigate to="/" replace />; // replace the current history entry user cant push back 
   }
-  // aslo later need to check the sessioncode is valid and navigate if not
+  const session = await validateSession(code);// session is valid check
+
+  if(!session.status){
+    showHotToast(session.message,"error")
+    return <Navigate to="/" replace />;
+  }
 
   return children;
 }
