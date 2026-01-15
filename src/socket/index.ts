1d5f594667acd01d1e5c3797b83abc289438fa6f
THARUNJP <tharunroshini71@gmail.com>
2026-01-15 17:04:28 +0530

    changes for media sockets wss

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,13 +1,14 @@
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_SIGNALING_URL;
+const SOCKET_URL = import.meta.env.VITE_SIGNALING_URL!;
+const MEDIA_SOCKET_URL = import.meta.env.MEDIA_SOCKET_URL!;
 
 export const socket: Socket = io(SOCKET_URL, {
   transports: ["websocket"],
   autoConnect: false,
 });
 
-export const mediaSocket: Socket = io(`${SOCKET_URL}/mediasoup`, {
+export const mediaSocket: Socket = io(MEDIA_SOCKET_URL, {
   transports: ["websocket"],
   autoConnect: false,
 });
