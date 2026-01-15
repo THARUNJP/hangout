0d372903da54d481ac7497bec7105e08f5b1b9aa
THARUNJP <tharunroshini71@gmail.com>
2026-01-15 17:18:22 +0530

    media soc check

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,14 +1,21 @@
 import { io, Socket } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.VITE_SIGNALING_URL;
-const MEDIA_SOCKET_URL = import.meta.env.MEDIA_SOCKET_URL;
 
 export const socket: Socket = io(SOCKET_URL, {
   transports: ["websocket"],
   autoConnect: false,
 });
 
-export const mediaSocket: Socket = io(MEDIA_SOCKET_URL, {
-  transports: ["websocket"],
-  autoConnect: false,
-});
+// export const mediaSocket: Socket = io(`${SOCKET_URL}/mediasoup`, {
+//   autoConnect: false,
+//   transports: ["websocket"],
+// });
+
+export const mediaSocket: Socket = io(
+  `wss://ec2-65-2-80-63.ap-south-1.compute.amazonaws.com/mediasoup`,
+  {
+    transports: ["websocket"],
+    autoConnect: false,
+  }
+);
