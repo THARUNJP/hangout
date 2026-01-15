fc593dbbedfae4411f7371ca21c7186849748aba
THARUNJP <tharunroshini71@gmail.com>
2026-01-15 17:10:19 +0530

    trigger redeploy

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_SIGNALING_URL!;
-const MEDIA_SOCKET_URL = import.meta.env.MEDIA_SOCKET_URL!;
+const SOCKET_URL = import.meta.env.VITE_SIGNALING_URL;
+const MEDIA_SOCKET_URL = import.meta.env.MEDIA_SOCKET_URL;
 
 export const socket: Socket = io(SOCKET_URL, {
   transports: ["websocket"],
